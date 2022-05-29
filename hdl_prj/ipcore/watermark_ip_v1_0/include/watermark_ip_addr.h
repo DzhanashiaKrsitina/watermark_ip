3b29a936587bf1dbd75e7c75a872a03fa25eb15c
DzhanashiaKrsitina <46706424+DzhanashiaKrsitina@users.noreply.github.com>
2022-05-29 17:40:41 +0300

    Add files via upload

--- hdl_prj/ipcore/watermark_ip_v1_0/include/watermark_ip_addr.h
+++ hdl_prj/ipcore/watermark_ip_v1_0/include/watermark_ip_addr.h
@@ -1,7 +1,7 @@
 /*
  * File Name:         hdl_prj\ipcore\watermark_ip_v1_0\include\watermark_ip_addr.h
  * Description:       C Header File
- * Created:           2022-05-03 00:49:48
+ * Created:           2022-05-12 17:02:14
 */
 
 #ifndef WATERMARK_IP_H_
@@ -9,7 +9,7 @@
 
 #define  IPCore_Reset_watermark_ip       0x0  //write 0x1 to bit 0 to reset IP core
 #define  IPCore_Enable_watermark_ip      0x4  //enabled (by default) when bit 0 is 0x1
-#define  IPCore_Timestamp_watermark_ip   0x8  //contains unique IP timestamp (yymmddHHMM): 2205030049
+#define  IPCore_Timestamp_watermark_ip   0x8  //contains unique IP timestamp (yymmddHHMM): 2205121702
 #define  pixel_in_Data_watermark_ip      0x100  //data register for Inport pixel_in
 #define  mes_in_Data_watermark_ip        0x104  //data register for Inport mes_in
 #define  pixel_out_Data_watermark_ip     0x108  //data register for Outport pixel_out
